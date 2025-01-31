e253c978e143ebd595657284f65d5027b0198311
miguelzackk <mzsilvaw@gmail.com>
2025-01-31 08:44:36 -0300

    finish-v1.1-backend-olho-vivo

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const busRoutes = require("./src/routes/busRoutes");
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/", busRoutes);
 
--- src/services/googleService.js
+++ src/services/googleService.js
@@ -1,49 +1,53 @@
 const axios = require("axios");
-const {
-  GOOGLE_API_KEY,
-  GOOGLE_GEOCODING_URL,
-  GOOGLE_DISTANCE_MATRIX_URL,
-} = require("../config/apiConfig");
+const { GOOGLE_API_KEY, GOOGLE_GEOCODING_URL, GOOGLE_DISTANCE_MATRIX_URL } = require("../config/apiConfig");
 
 async function buscarCoordenadasEndereco(endereco) {
-  try {
-    const response = await axios.get(GOOGLE_GEOCODING_URL, {
-      params: { address: endereco, key: GOOGLE_API_KEY },
-    });
-    if (response.data.results.length > 0) {
-      return response.data.results[0].geometry.location;
-    } else {
-      return null;
+    try {
+        const response = await axios.get(GOOGLE_GEOCODING_URL, {
+            params: { address: endereco, key: GOOGLE_API_KEY },
+        });
+        if (response.data.results.length > 0) {
+            return response.data.results[0].geometry.location;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error("Erro ao buscar coordenadas:", error.message);
+        return null;
     }
-  } catch (error) {
-    console.error("Erro ao buscar coordenadas:", error.message);
-    return null;
-  }
 }
 
-async function calcularTempoComGoogle(
-  origemLat,
-  origemLng,
-  destinoLat,
-  destinoLng
-) {
-  try {
-    const response = await axios.get(GOOGLE_DISTANCE_MATRIX_URL, {
-      params: {
-        origins: `${origemLat},${origemLng}`,
-        destinations: `${destinoLat},${destinoLng}`,
-        key: GOOGLE_API_KEY,
-        mode: "driving",
-      },
-    });
-    return (
-      Math.ceil(response.data.rows[0]?.elements[0]?.duration?.value / 60) ||
-      null
-    );
-  } catch (error) {
-    console.error("Erro ao calcular tempo de chegada:", error.message);
-    return null;
-  }
+async function calcularTempoComGoogle(origemLat, origemLng, destinoLat, destinoLng) {
+    try {
+        const response = await axios.get(GOOGLE_DISTANCE_MATRIX_URL, {
+            params: {
+                origins: `${origemLat},${origemLng}`,
+                destinations: `${destinoLat},${destinoLng}`,
+                key: GOOGLE_API_KEY,
+                mode: "driving",
+            },
+        });
+        return Math.ceil(response.data.rows[0]?.elements[0]?.duration?.value / 60) || null;
+    } catch (error) {
+        console.error("Erro ao calcular tempo de chegada:", error.message);
+        return null;
+    }
 }
 
-module.exports = { buscarCoordenadasEndereco, calcularTempoComGoogle };
+async function converterCoordenadasParaEndereco(latitude, longitude) {
+    try {
+        const response = await axios.get(GOOGLE_GEOCODING_URL, {
+            params: { latlng: `${latitude},${longitude}`, key: GOOGLE_API_KEY },
+        });
+        if (response.data.results.length > 0) {
+            return response.data.results[0].formatted_address;
+        } else {
+            return "Endereço não encontrado";
+        }
+    } catch (error) {
+        console.error("Erro ao converter coordenadas em endereço:", error.message);
+        return "Erro ao obter endereço";
+    }
+}
+
+module.exports = { buscarCoordenadasEndereco, calcularTempoComGoogle, converterCoordenadasParaEndereco };
