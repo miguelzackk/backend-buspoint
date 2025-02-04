0097a2726d3d50713aa3f9b7fed4162cdfaefe16
miguelzackk <mzsilvaw@gmail.com>
2025-02-04 10:01:02 -0300

    return-v5

--- src/services/googleService.js
+++ src/services/googleService.js
@@ -1,50 +1,52 @@
 //googleService.js
 const axios = require("axios");
-const { GOOGLE_API_KEY, GOOGLE_GEOCODING_URL, GOOGLE_DISTANCE_MATRIX_URL } = require("../config/apiConfig");
+const {
+  GOOGLE_API_KEY,
+  GOOGLE_GEOCODING_URL,
+  GOOGLE_DISTANCE_MATRIX_URL,
+  GOOGLE_TRANSIT_URL,
+} = require("../config/apiConfig");
 
 async function buscarCoordenadasEndereco(endereco) {
   try {
     const response = await axios.get(GOOGLE_GEOCODING_URL, {
       params: { address: endereco, key: GOOGLE_API_KEY },
     });
-    return response.data.results[0]?.geometry.location || null;
+    if (response.data.results.length > 0) {
+      return response.data.results[0].geometry.location;
+    } else {
+      return null;
+    }
   } catch (error) {
     console.error("Erro ao buscar coordenadas:", error.message);
     return null;
   }
 }
 
-async function calcularTempoComGoogle(origemLat, origemLng, destinoLat, destinoLng) {
+async function calcularTempoComGoogle(
+  origemLat,
+  origemLng,
+  destinoLat,
+  destinoLng
+) {
   try {
     const response = await axios.get(GOOGLE_DISTANCE_MATRIX_URL, {
       params: {
         origins: `${origemLat},${origemLng}`,
         destinations: `${destinoLat},${destinoLng}`,
         key: GOOGLE_API_KEY,
-        mode: "transit",
+        travelMode: "TRANSIT",
+        transitPreferences: {
+          allowedTravelModes: ["BUS"] 
+        }
       },
     });
-    return Math.ceil(response.data.rows[0]?.elements[0]?.duration?.value / 60) || null;
+    return (
+      Math.ceil(response.data.rows[0]?.elements[0]?.duration?.value / 60) ||
+      null
+    );
   } catch (error) {
     console.error("Erro ao calcular tempo de chegada:", error.message);
-    return null;
-  }
-}
-
-async function consultarRotaOnibus(origem, destino) {
-  try {
-    const response = await axios.get("https://maps.googleapis.com/maps/api/directions/json", {
-      params: {
-        origin: origem,
-        destination: destino,
-        mode: "transit",
-        transit_mode: "bus",
-        key: GOOGLE_API_KEY,
-      },
-    });
-    return response.data.routes[0] || null;
-  } catch (error) {
-    console.error("Erro ao consultar a rota de ônibus:", error.message);
     return null;
   }
 }
@@ -65,10 +67,33 @@ async function converterCoordenadasParaEndereco(latitude, longitude) {
   }
 }
 
+async function consultarRotaOnibus(origem, destino) {
+  try {
+    const response = await axios.post(GOOGLE_TRANSIT_URL, {
+      origin: {
+        address: origem
+      },
+      destination: {
+        address: destino
+      },
+      travelMode: "TRANSIT",
+      transitPreferences: {
+        allowedTravelModes: ["BUS"],
+      },
+      computeAlternativeRoutes: true,
+      key: GOOGLE_API_KEY,
+    });
+    console.log("Rota encontrada: ", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao consultar a rota de transporte público:", error.message);
+    return null;
+  }
+}
 
 module.exports = {
   buscarCoordenadasEndereco,
   calcularTempoComGoogle,
-  consultarRotaOnibus,
   converterCoordenadasParaEndereco,
+  consultarRotaOnibus,
 };
